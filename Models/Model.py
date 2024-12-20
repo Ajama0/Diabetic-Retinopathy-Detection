9e26b61f5c4a16f0def957aee98bbeeffb3819f9
Ajama0 <abasjama04@hotmail.com>
2024-12-20 09:35:52 +0000

    set up dependencies and load in data

---
+++ Models/Model.py
@@ -0,0 +1,35 @@
+import os
+import cv2
+from tqdm import tqdm
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torchvision
+import pandas as pd
+
+
+#once data is processed set to False to save as pickle file
+REBUILD_DATA = True
+#path to the trainset images
+path = "Dataset/raw/train_images"
+#path to 
+train_csv_path = "Dataset/raw/test_images"
+IMG_SIZE = 224
+training_data = []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
