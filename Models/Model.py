1b7b4a763ce9c38bb0419ba1499e3c74c3df1473
Ajama0 <abasjama04@hotmail.com>
2024-12-21 12:40:38 +0000

    validation checks on train csv

--- Models/Model.py
+++ Models/Model.py
@@ -6,9 +6,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch
 import pandas as pd
-from torch.utils.data import dataloader, Dataset
+from torch.utils.data import DataLoader, Dataset
 from torchvision.io import read_image
 from torchvision import transforms
+from pathlib import Path
 
 
 
@@ -75,14 +76,34 @@ class DiabeticRetinopathyDataset(Dataset):
     def __init__(self, img_dir,annotations_file,tranform = None):
         #the directory point to the train images
         self.img_dir  = img_dir
-        
         #this refers to the csv file representing image and labels, we can later create an instance and pass in any csv and dataset
-        self.img_labels= annotations_file
+        self.annotations_file = annotations_file
+        
+        #validate csv file before assignment
+        self.img_labels= self.csv_check()
         #this allows us to perform augmentation and other techniques on our input(images)
         self.transform = tranform
-        
 
-    
+
+        #check if the csv file exists
+    def csv_check(self):
+        #check to ensure the path file is valid
+        if not os.path.isfile(self.annotations_file):
+            raise FileNotFoundError(f"CSV file '{self.annotations_file}' not found.")
+            
+        #read file and throw an exception if any error occurs    
+        try:
+            read_file = pd.read_csv(self.annotations_file)
+        except Exception as e:
+            raise FileNotFoundError(f"Error reading '{self.annotations_file}': {e}")
+
+        #ensure csv file is not empty
+        if read_file.empty:
+            raise FileNotFoundError(f"File '{self.annotations_file}' is empty.")
+            
+        return read_file
+
+        
     #implementing len method to return length of dataset, used when iterating over dataloader
     def __len__(self):
         return len(self.img_labels) 
@@ -99,11 +120,15 @@ class DiabeticRetinopathyDataset(Dataset):
         img_label = self.img_labels.iloc[idx,1]
 
         #check if any transformations are to be applied to each image
+        #this will transform our np array into a tensor and normalize the image pixel from [0-255]
         if self.transform:
             image = self.transform(img)
 
         #one hot encoding will be performed after passing to to the dataloader
         return image, img_label
+    
+
+
     
 
 
