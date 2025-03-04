0e486b117f778431d66af522eaf66fba0ad64ed1
ajama0 <abasjama04@hotmail.com>
2025-03-04 18:28:26 +0300

    begin cropping script

---
+++ Scripts/crop.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import numpy as np
+from tqdm import tqdm
+import cv2
+import io
+
+#resize, crop to remove noise(black borders)
+#this flag allows to differentiate between dev dataset and final dataset
+DEV_SET = True
+class Crop:
+    def __init__(self, df):
+        self.df = df
+
+
+
+    """
+    images to be cropped will be stored in a new location
+    dynamic cropping, because some images may be 
+    """
+    def crop_images(self, path, resize):
+        
+
+
